58bf823cdee824880118cd5c57a1bd12841c995a
Kyle Li <kyleseanli@gmail.com>
2023-05-23 19:00:05 -0400

    Implemented Whisper

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+# GPT Config
 MODEL_ID = 'gpt-3.5-turbo'
 TEMPERATURE = 0.2
 MAX_TOKENS = 4096
+
+# Whisper Config
+conversation_type = "A Hospital Visit"
+language = "English"
--- whisper.py
+++ whisper.py
@@ -1,4 +1,20 @@
 import openai
+from config import conversation_type, language
 
-def initalize_whisper(api_key):
-    openai.api_key = api_key
+def convert_audio(api_key):
+    media_file_path = 'testaudio.m4a'
+    media_file = open(media_file_path, 'rb')
+    model_id = 'whisper-1'
+
+    response = openai.Audio.transcribe(
+        api_key = api_key,
+        model = model_id,
+        file = media_file,
+        prompt = f"This is a conversation about {conversation_type} in {language}."
+        response_format = 'text'
+    )
+    print(response)
+
+if __name__ == '__main__':
+    api_key = input("API KEY: ")
+    convert_audio(api_key)
