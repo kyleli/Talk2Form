ee074c6f2a1393594e295ee848f1eeb0ed16850b
Kyle Li <kyleseanli@gmail.com>
2023-06-15 21:59:00 -0400

    Working version of record_audio in console

--- talktoform/website/views.py
+++ talktoform/website/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequest
 from .models import FormTemplate, User, Question, Form, FormResponse, AudioFile
+from utils.record import record_audio
 
 # Create your views here.
 def index(request):
@@ -66,8 +67,8 @@ def usersettings(request):
     return render(request, 'usersettings.html')
 
 @login_required
-def editform(request, form_id):
-    form_template = get_object_or_404(FormTemplate, id=form_id)
+def editform(request, form_template_id):
+    form_template = get_object_or_404(FormTemplate, id=form_template_id)
     if form_template.user != request.user: # Only the forms attached to the user can be accessed
         return HttpResponseForbidden("You don't have permission to access this form.")
 
@@ -95,63 +96,72 @@ def create_default_form_template(request):
     return render(request, 'dashboard.html')
 
 @login_required
-def edit_template_title(request, form_id):
-    form_template = get_object_or_404(FormTemplate, id=form_id)
+def edit_template_title(request, form_template_id):
+    form_template = get_object_or_404(FormTemplate, id=form_template_id)
     if form_template.user != request.user:
         return HttpResponseForbidden("You don't have permission to edit this form.")
 
     return render(request, 'editform.html', {'form_template': form_template, 'editing': True})
 
 @login_required
-def save_template_title(request, form_id):
-    form_template = get_object_or_404(FormTemplate, id=form_id)
+def save_template_title(request, form_template_id):
+    form_template = get_object_or_404(FormTemplate, id=form_template_id)
     if form_template.user != request.user:
         return HttpResponseForbidden("You don't have permission to save this form.")
 
     if request.method == 'POST':
         form_template.title = request.POST.get('title')
         form_template.save()
-        return redirect('editform', form_id=form_template.id)
+        return redirect('editform', form_template_id=form_template.id)
 
     return HttpResponseBadRequest("Invalid request method.")
 
 @login_required
-def create_question(request, form_id):
+def create_question(request, form_template_id):
     if request.method == 'POST':
         title = 'New Question'
-        template = get_object_or_404(FormTemplate, id=form_id)
+        template = get_object_or_404(FormTemplate, id=form_template_id)
         Question.objects.create(template=template, question=title)
-        return redirect('editform', form_id=form_id)
-    return redirect('editform', form_id=form_id)
+        return redirect('editform', form_template_id=form_template_id)
+    return redirect('editform', form_template_id=form_template_id)
 
 @login_required
-def edit_question(request, form_id, question_id):
+def edit_question(request, form_template_id, question_id):
     question = get_object_or_404(Question, id=question_id)
-    form_template = get_object_or_404(FormTemplate, id=form_id)
+    form_template = get_object_or_404(FormTemplate, id=form_template_id)
     if form_template.user != request.user:
         return HttpResponseForbidden("You don't have permission to edit this form.")
 
     if request.method == 'POST':
         question.question = request.POST.get('question')
         question.save()
-        return redirect('editform', form_id=form_template.id)
+        return redirect('editform', form_template_id=form_template.id)
 
     return render(request, 'editform.html', {'form_template': form_template, 'question': question, 'editing': True})
 
 @login_required
-def save_question(request, form_id, question_id):
+def save_question(request, form_template_id, question_id):
     question = get_object_or_404(Question, id=question_id)
-    form_template = get_object_or_404(FormTemplate, id=form_id)
+    form_template = get_object_or_404(FormTemplate, id=form_template_id)
     if form_template.user != request.user:
         return HttpResponseForbidden("You don't have permission to save this form.")
 
     if request.method == 'POST':
         question.question = request.POST.get('question')
         question.save()
-        return redirect('editform', form_id=form_template.id)
+        return redirect('editform', form_template_id=form_template.id)
 
     return HttpResponseBadRequest("Invalid request method.")
 
 @login_required
 def record_view(request, form_template_id):
-    return render(request, 'record.html')
+    if request.method == 'POST':
+        form_id = request.POST.get('form_id')
+        audio_data = request.FILES.get('audio_data')
+        record_audio(form_id=form_id)
+        audio_file = AudioFile(form_id=form_id, audio_file=audio_data)
+        audio_file.save()
+
+        return redirect('form_template_detail', form_template_id=form_id)
+
+    return render(request, 'record.html', {'form_template_id': form_template_id})
