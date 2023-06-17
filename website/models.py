fa3111f13079d8362a4aa58a95790d817987bba6
Kyle Li <kyleseanli@gmail.com>
2023-06-17 06:54:18 -0400

    Added user

--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
