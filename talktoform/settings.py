d70992db547182e5103c34d118c9df1a0adf0f34
Kyle Li <kyleseanli@gmail.com>
2023-06-17 06:32:06 -0400

    Assigned Database

--- talktoform/settings.py
+++ talktoform/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'talktoform.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/talktoformdb',
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
     }
