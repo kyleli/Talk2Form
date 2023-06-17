7f437d6921a641a3d0605e2973e5fd204d0a13f7
Kyle Li <kyleseanli@gmail.com>
2023-06-17 07:01:55 -0400

    Removed user from admin register

--- website/admin.py
+++ website/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import User, FormTemplate, Form, Question, FormResponse, AudioFile, FormConfig
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(FormTemplate)
 admin.site.register(Form)
 admin.site.register(Question)
