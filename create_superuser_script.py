33c06d2749616e9f217e8d52d99d83e1e8549175
Kyle Li <kyleseanli@gmail.com>
2023-06-17 07:48:57 -0400

    added talktoform to superuser

--- create_superuser_script.py
+++ create_superuser_script.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "talktoform.settings")
 django.setup()
 
 from django.contrib.auth.models import User
