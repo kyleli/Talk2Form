e7ba33653c273787803d0adad55d503694e64f5e
Kyle Li <kyleseanli@gmail.com>
2023-06-19 21:39:47 -0400

    Removed CSRF_TRUSTED_ORIGINS

--- talktoform/settings.py
+++ talktoform/settings.py
@@ -33,8 +33,6 @@ DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "127.0.0.1,localhost").split(",")
-
 # Application definition
 
 INSTALLED_APPS = [
