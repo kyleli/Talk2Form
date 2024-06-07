186cd6e49d090976fb6f3f3fffef3fcae5273a8a
Kyle Li <kyleseanli@gmail.com>
2024-06-07 11:05:04 -0400

    Debugging out whisper info

--- website/utils/whisper.py
+++ website/utils/whisper.py
@@ -158,5 +158,6 @@ def convert_audio(audio_bytes, form_instance):
             prompt=f"{conversation_type}",
             response_format='text'
         )
-        print(response)
+        print("Model ID" + audio_recognition_model_id + "Language Code: " + language_code + "Conversation Type: " + conversation_type)
+
         return response
